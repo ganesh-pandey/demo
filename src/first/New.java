5d2d1f5509e98d214730ddf5e94e6b20cbc74581
ganesh-pandey <ganesh.pandey_cs19@gla.ac.in>
2020-07-26 04:25:14 -0700

    sum of tow no.

---
+++ src/first/New.java
@@ -0,0 +1,14 @@
+package first;
+import java.util.Scanner;
+
+public class New {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a the number");
+        int a= sc.nextInt();
+        int b =sc.nextInt();
+        int sum;
+        sum =a+b;
+        System.out.println("sum of the digit is = "+sum);
+    }
+}
